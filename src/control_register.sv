d0f5e3d69395393c0fb6346ae9732aa11e63bdd7
Isaiah Grace <igrace@purdue.edu>
2019-09-30 14:18:15 -0400

    outline of control_register.sv

---
+++ src/control_register.sv
@@ -0,0 +1,28 @@
+/* 
+ Isaiah Grace
+ igrace@purdue.edu
+ 
+ This module controls the orientation and status of the test structures
+ */
+
+// Includes
+include "POLI_types_pkg.vh"
+include "control_register_if.vh"
+
+module control_register (
+			 input logic CLK,
+			 input logic nRST,
+			 control_register_if crif
+			 );
+   // Import types
+   import POLI_types_pkg::*;
+
+   // local signals and registers
+   
+   // combinational logic
+
+   // sequential logic
+   
+endmodule // control_register
+
+     
