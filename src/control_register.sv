ff7716ab4cfe8dba5db57c4e169ff821b49855ba
Isaiah Grace <igrace@purdue.edu>
2019-10-11 15:47:41 -0400

    first pass at control_register logic

--- src/control_register.sv
+++ src/control_register.sv
@@ -17,12 +17,56 @@ module control_register (
    // Import types
    import POLI_types_pkg::*;
 
-   // local signals and registers
+   // combinational logic for read_data
+   always_comb
+     begin
+	casez (crif.register_select)
+	  BAD_ADDR:         crif.read_data = 32'hbad1bad1;
+	  NAND_NOR_CONTROL: crif.read_data = {31'b0, crif.NAND_NOR_orient};
+	  NAND_NOR_INPUT:   crif.read_data = {30'b0, crif.NAND_NOR_b, crif.NAND_NOR_a};
+	  NAND_NOR_OUTPUT:  crif.read_data = {31'b0, crif.NAND_NOR_out};
+	  XOR_BUF_CONTROL:  crif.read_data = {31'b0, crif.XOR_BUF_orient};
+	  XOR_BUF_INPUT:    crif.read_data = {30'b0, crif.XOR_BUF_b, crif.XOR_BUF_a};
+	  XOR_BUF_OUTPUT:   crif.read_data = {31'b0, crif.XOR_BUF_out};
+	  CRC_CONTROL:      crif.read_data = {30'b0, crif.crc_reset, crif.crc_start};
+	  CRC_CONFIG:       crif.read_data = crif.crc_orient;
+	  CRC_STATUS:       crif.read_data = {31'b0, crif.crc_ready};
+	  CRC_INPUT:        crif.read_data = crif.crc_data_in;
+	  CRC_OUTPUT:       crif.read_data = crif.crc_data_out;
+	  default:          crif.read_data = 32'hbad2bad2;
+	endcase // casez (register_select)
+     end // always_comb
    
-   // combinational logic
-
-   // sequential logic
-   
+   // sequential logic for registers and write_data
+   always_ff @(posedge CLK, negedge nRST)
+     begin
+	if (!nRST)
+	  begin
+	     crif.crc_data_in     <= '0;
+	     crif.crc_reset       <= '0;
+	     crif.crc_start       <= '0;
+	     crif.crc_orient      <= '0;
+	     crif.NAND_NOR_a      <= '0;
+	     crif.NAND_NOR_b      <= '0;
+	     crif.NAND_NOR_orient <= '0;
+	     crif.XOR_BUF_a       <= '0;
+	     crif.XOR_BUF_b       <= '0;
+	     crif.XOR_BUF_orient  <= '0;
+	  end // if (!nRST)
+	else
+	  begin
+	     crif.crc_data_in     <= crif.write_enable && crif.register_select == CRC_INPUT        ? crif.write_data    : crif.crc_data_in;
+	     crif.crc_start       <= crif.write_enable && crif.register_select == CRC_CONTROL      ? crif.write_data[0] : crif.crc_start;
+	     crif.crc_reset       <= crif.write_enable && crif.register_select == CRC_CONTROL      ? crif.write_data[1] : crif.crc_reset;
+	     crif.crc_orient      <= crif.write_enable && crif.register_select == CRC_CONFIG       ? crif.write_data    : crif.crc_orient;
+	     crif.NAND_NOR_a      <= crif.write_enable && crif.register_select == NAND_NOR_INPUT   ? crif.write_data[0] : crif.NAND_NOR_a;
+	     crif.NAND_NOR_b      <= crif.write_enable && crif.register_select == NAND_NOR_INPUT   ? crif.write_data[1] : crif.NAND_NOR_b;
+	     crif.NAND_NOR_orient <= crif.write_enable && crif.register_select == NAND_NOR_CONTROL ? crif.write_data[0] : crif.NAND_NOR_orient;
+	     crif.XOR_BUF_a       <= crif.write_enable && crif.register_select == XOR_BUF_INPUT    ? crif.write_data[0] : crif.XOR_BUF_a;
+	     crif.XOR_BUF_b       <= crif.write_enable && crif.register_select == XOR_BUF_INPUT    ? crif.write_data[1] : crif.XOR_BUF_b;
+	     crif.XOR_BUF_orient  <= crif.write_enable && crif.register_select == XOR_BUF_CONTROL  ? crif.write_data[0] : crif.XOR_BUF_orient;
+	  end // else: !if(!nRST)
+     end // always_ff @ (posedge CLK, negedge nRST)
 endmodule // control_register
 
      
