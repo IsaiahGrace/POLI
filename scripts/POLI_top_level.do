c64cd2242996e9ac641f64d354dec6f88a998a3f
Isaiah Grace <igrace@purdue.edu>
2019-10-14 15:02:38 -0400

    Top level testbench demonstrates Logic gate truth table

---
+++ scripts/POLI_top_level.do
@@ -0,0 +1,64 @@
+onerror {resume}
+quietly WaveActivateNextPane {} 0
+add wave -noupdate -color Gold /POLI_top_level_tb/CLK
+add wave -noupdate -color Gold /POLI_top_level_tb/nRST
+add wave -noupdate -color Gold /POLI_top_level_tb/PROG/test_case
+add wave -noupdate -color Gold /POLI_top_level_tb/PROG/test_name
+add wave -noupdate -color Gold /POLI_top_level_tb/PROG/transfer_phase
+add wave -noupdate -expand -group APB -color {Violet Red} /POLI_top_level_tb/PWDATA
+add wave -noupdate -expand -group APB -color {Violet Red} /POLI_top_level_tb/PADDR
+add wave -noupdate -expand -group APB -color {Violet Red} /POLI_top_level_tb/PRDATA
+add wave -noupdate -expand -group APB -color {Violet Red} /POLI_top_level_tb/PWRITE
+add wave -noupdate -expand -group APB -color {Violet Red} /POLI_top_level_tb/PSEL
+add wave -noupdate -expand -group APB -color {Violet Red} /POLI_top_level_tb/PENABLE
+add wave -noupdate -expand -group APB -color {Violet Red} /POLI_top_level_tb/PREADY
+add wave -noupdate -expand -group CRIF -color {Lime Green} /POLI_top_level_tb/DUT/crif/read_data
+add wave -noupdate -expand -group CRIF -color {Lime Green} /POLI_top_level_tb/DUT/crif/write_data
+add wave -noupdate -expand -group CRIF -color {Lime Green} /POLI_top_level_tb/DUT/crif/write_enable
+add wave -noupdate -expand -group CRIF -color {Lime Green} /POLI_top_level_tb/DUT/crif/register_select
+add wave -noupdate -expand -group CRIF -color {Lime Green} /POLI_top_level_tb/DUT/crif/crc_data_in
+add wave -noupdate -expand -group CRIF -color {Lime Green} /POLI_top_level_tb/DUT/crif/crc_reset
+add wave -noupdate -expand -group CRIF -color {Lime Green} /POLI_top_level_tb/DUT/crif/crc_start
+add wave -noupdate -expand -group CRIF -color {Lime Green} /POLI_top_level_tb/DUT/crif/crc_orient
+add wave -noupdate -expand -group CRIF -color {Lime Green} /POLI_top_level_tb/DUT/crif/crc_data_out
+add wave -noupdate -expand -group CRIF -color {Lime Green} /POLI_top_level_tb/DUT/crif/crc_ready
+add wave -noupdate -expand -group CRIF -color {Lime Green} /POLI_top_level_tb/DUT/crif/NAND_NOR_out
+add wave -noupdate -expand -group CRIF -color {Lime Green} /POLI_top_level_tb/DUT/crif/NAND_NOR_a
+add wave -noupdate -expand -group CRIF -color {Lime Green} /POLI_top_level_tb/DUT/crif/NAND_NOR_b
+add wave -noupdate -expand -group CRIF -color {Lime Green} /POLI_top_level_tb/DUT/crif/NAND_NOR_orient
+add wave -noupdate -expand -group CRIF -color {Lime Green} /POLI_top_level_tb/DUT/crif/XOR_BUF_out
+add wave -noupdate -expand -group CRIF -color {Lime Green} /POLI_top_level_tb/DUT/crif/XOR_BUF_a
+add wave -noupdate -expand -group CRIF -color {Lime Green} /POLI_top_level_tb/DUT/crif/XOR_BUF_b
+add wave -noupdate -expand -group CRIF -color {Lime Green} /POLI_top_level_tb/DUT/crif/XOR_BUF_orient
+add wave -noupdate -expand -group CRCIF -color {Cadet Blue} /POLI_top_level_tb/DUT/crcif/crc_data_in
+add wave -noupdate -expand -group CRCIF -color {Cadet Blue} /POLI_top_level_tb/DUT/crcif/crc_reset
+add wave -noupdate -expand -group CRCIF -color {Cadet Blue} /POLI_top_level_tb/DUT/crcif/crc_start
+add wave -noupdate -expand -group CRCIF -color {Cadet Blue} /POLI_top_level_tb/DUT/crcif/crc_orient
+add wave -noupdate -expand -group CRCIF -color {Cadet Blue} /POLI_top_level_tb/DUT/crcif/crc_data_out
+add wave -noupdate -expand -group CRCIF -color {Cadet Blue} /POLI_top_level_tb/DUT/crcif/crc_ready
+add wave -noupdate -expand -group NAND_NOR -color {Cornflower Blue} /POLI_top_level_tb/DUT/NAND_NOR/A
+add wave -noupdate -expand -group NAND_NOR -color {Cornflower Blue} /POLI_top_level_tb/DUT/NAND_NOR/B
+add wave -noupdate -expand -group NAND_NOR -color {Cornflower Blue} /POLI_top_level_tb/DUT/NAND_NOR/orient
+add wave -noupdate -expand -group NAND_NOR -color {Cornflower Blue} /POLI_top_level_tb/DUT/NAND_NOR/X
+add wave -noupdate -expand -group XOR_BUF -color {Dark Orchid} /POLI_top_level_tb/DUT/XOR_BUF/A
+add wave -noupdate -expand -group XOR_BUF -color {Dark Orchid} /POLI_top_level_tb/DUT/XOR_BUF/B
+add wave -noupdate -expand -group XOR_BUF -color {Dark Orchid} /POLI_top_level_tb/DUT/XOR_BUF/orient
+add wave -noupdate -expand -group XOR_BUF -color {Dark Orchid} /POLI_top_level_tb/DUT/XOR_BUF/X
+TreeUpdate [SetDefaultTree]
+WaveRestoreCursors {{Cursor 1} {32 ns} 0}
+quietly wave cursor active 1
+configure wave -namecolwidth 175
+configure wave -valuecolwidth 100
+configure wave -justifyvalue left
+configure wave -signalnamewidth 1
+configure wave -snapdistance 10
+configure wave -datasetprefix 0
+configure wave -rowmargin 4
+configure wave -childrowmargin 2
+configure wave -gridoffset 0
+configure wave -gridperiod 1
+configure wave -griddelta 40
+configure wave -timeline 0
+configure wave -timelineunits ns
+update
+WaveRestoreZoom {0 ns} {137 ns}
--- testbench/POLI_top_level_tb.sv
+++ testbench/POLI_top_level_tb.sv
@@ -8,6 +8,9 @@
 // Data types
 `include "POLI_types_pkg.vh"
 
+// Import types
+import POLI_types_pkg::*;
+
 `timescale 1 ns / 1 ns
 
 module POLI_top_level_tb;
@@ -63,6 +66,8 @@ program test
    // Test signals
    int 		test_case;
    string 	test_name;
+   string 	transfer_phase;
+   
 
    task reset_DUT;
       begin
@@ -82,9 +87,167 @@ program test
    endtask // reset_DUT
 
    // APB write data task
+   task apb_write;
+      input [WORD_SIZE-1:0] write_data;
+      input [WORD_SIZE-1:0] address;
+      input 		    select;   
+      begin
+	 // Expected signals
+	 logic exp_PREADY;
 
-   // APB read data task
+	 @(posedge CLK);
+	 #(0.5)
+	 // Setup phase of transfer
+	 PADDR = address;
+	 PWRITE = 1'b1;
+	 PSEL = select;
+	 PENABLE = 1'b0;
+	 PWDATA = write_data;
+	 transfer_phase = select ? "Setup" : "NO SEL";
+	 	 
+	 @(negedge CLK);
+	 // Calculate setup expected outputs
+	 exp_PREADY = 1'b0;
+
+	 // Check apb_slave outputs during Setup
+	 assert (PREADY == exp_PREADY)
+	   else $error("PREADY != %d during: %s", exp_PREADY, test_name);
+
+	 @(posedge CLK);
+	 #(0.5)
+	 // Access phase of transfer
+	 PADDR = address;
+	 PWRITE = 1'b1;
+	 PSEL = select;
+	 PENABLE = 1'b1;
+	 PWDATA = write_data;
+	 transfer_phase = select ? "Access" : "NO SEL";
+	 
+	 @(negedge CLK);
+	 // Calculate access expected outputs
+	 exp_PREADY = select;
+	 
+	 // Check apb_slave outputs during Access
+	 assert (PREADY == exp_PREADY)
+	   else $error("PREADY != %d during: %s", exp_PREADY, test_name);
+      end      
+   endtask // apb_write
+
    
+   // APB read and check data task
+   task apb_read;
+      input [WORD_SIZE-1:0] exp_data;
+      input [WORD_SIZE-1:0] address;
+      input 		    select;
+      begin
+	 // Expected signals from apb_slave
+	 logic 		       exp_PREADY;
+	 logic [WORD_SIZE-1:0] exp_PRDATA;
+
+	 @(posedge CLK);
+	 #(0.5)
+	 // Setup phase of transfer
+	 PADDR = address;
+	 PWRITE = 1'b0;
+	 PSEL = select;
+	 PENABLE = 1'b0;
+	 transfer_phase = select ? "Setup" : "NO SEL";
+	 	 
+	 @(negedge CLK);
+	 // Calculate setup expected outputs
+	 exp_PREADY = 1'b0;
+
+	 // Check apb_slave outputs during Setup
+	 assert (PREADY == exp_PREADY)
+	   else $error("PREADY != %d during: %s", exp_PREADY, test_name);
+
+	 @(posedge CLK);
+	 #(0.5)
+	 // Access phase of transfer
+	 PADDR = address;
+	 PWRITE = 1'b0;
+	 PSEL = select;
+	 PENABLE = 1'b1;
+	 transfer_phase = select ? "Access" : "NO SEL";
+
+	 @(negedge CLK);
+	 // Calculate access expected outputs
+	 exp_PREADY = select;
+	 
+	 // Check apb_slave outputs during Access
+	 assert (PREADY == exp_PREADY)
+	   else $error("PREADY != %d during: %s", exp_PREADY, test_name);
+	 assert (PRDATA == exp_data)
+	   else $error("PRDATA != %h during: %s", exp_data, test_name);
+      end
+   endtask // apb_read
    
+
+   logic [3:0] 		       NAND_NOR_table [7:0];
+   logic [3:0] 		       XOR_BUF_table [7:0];
    
-  
+   //                            OBAX
+   assign NAND_NOR_table[0] = 4'b0001;
+   assign NAND_NOR_table[1] = 4'b0010;
+   assign NAND_NOR_table[2] = 4'b0100;
+   assign NAND_NOR_table[3] = 4'b0110;
+   assign NAND_NOR_table[4] = 4'b1001;
+   assign NAND_NOR_table[5] = 4'b1011;
+   assign NAND_NOR_table[6] = 4'b1101;
+   assign NAND_NOR_table[7] = 4'b1110;
+   
+   //                           OBAX
+   assign XOR_BUF_table[0] = 4'b0000;
+   assign XOR_BUF_table[1] = 4'b0011;
+   assign XOR_BUF_table[2] = 4'b0100;
+   assign XOR_BUF_table[3] = 4'b0111;
+   assign XOR_BUF_table[4] = 4'b1000;
+   assign XOR_BUF_table[5] = 4'b1011;
+   assign XOR_BUF_table[6] = 4'b1101;
+   assign XOR_BUF_table[7] = 4'b1110;
+
+   // Main Test sequence
+   initial
+     begin
+	// RESET DUT
+	reset_DUT();
+
+	// Demonstrate NAND_NOR Truth Table
+	test_case++;
+	test_name = "NAND_NOR Truth Table";
+		
+	for (int i = 0; i < 8; i++)
+	  begin
+	     apb_write(.write_data(NAND_NOR_table[i][3]),
+		       .address(NAND_NOR_CONTROL_ADDR),
+		       .select(1'b1));
+	     apb_write(.write_data(NAND_NOR_table[i][2:1]),
+		       .address(NAND_NOR_INPUT_ADDR),
+		       .select(1'b1));
+	     apb_read(.exp_data(NAND_NOR_table[i][0]),
+		      .address(NAND_NOR_OUTPUT_ADDR),
+		      .select(1'b1));
+	  end // for (int i = 0; i < 8; i++)
+
+	
+	// Demonstrate XOR_BUF Truth Table
+	test_case++;
+	test_name = "XOR_BUF Truth Table";
+	
+	for (int i = 0; i < 8; i++)
+	  begin
+	     apb_write(.write_data(XOR_BUF_table[i][3]),
+		       .address(XOR_BUF_CONTROL_ADDR),
+		       .select(1'b1));
+	     apb_write(.write_data(XOR_BUF_table[i][2:1]),
+		       .address(XOR_BUF_INPUT_ADDR),
+		       .select(1'b1));
+	     apb_read(.exp_data(XOR_BUF_table[i][0]),
+		      .address(XOR_BUF_OUTPUT_ADDR),
+		      .select(1'b1));
+	  end // for (int i = 0; i < 8; i++)
+
+	 
+     end // initial begin
+      
+endprogram // test
